9400ea986c5e29a4faee0c96d0c25418a4dbd8b9
Arpit Shukla <arpitnshukla@gmail.com>
2021-04-28 21:49:34 +0530

    Created Rinkeby Test Environment and tested investment functions in backend.

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,35 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./getWeb3";
+import comptrollerAbi from "./comptrollerAbi.json";
+import cETHAbi from "./cETHAbi.json";
+import cTokenAbi from "./cTokenAbi.json"
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { loaded:false };
 
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+      this.web3 = await getWeb3();
 
       // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+      this.accounts = await this.web3.eth.getAccounts();
 
       // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+      this.networkId = await this.web3.eth.net.getId();
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      //rinkeby testnet contracts
+      this.comptrollerInstance = new this.web3.eth.Contract(comptrollerAbi,"0x2eaa9d77ae4d8f9cdd9faacd44016e746485bddb");
+      this.cDAIInstance = new this.web3.eth.Contract(cTokenAbi,"0x6d7f0754ffeb405d23c51ce938289d4835be3b14");
+      this.cETHInstance = new this.web3.eth.Contract(cETHAbi,"0xd6801a1dffcd0a410336ef88def4320d6df1883e");
+      this.cTokenAddresses = ["0x6d7f0754ffeb405d23c51ce938289d4835be3b14","0xd6801a1dffcd0a410336ef88def4320d6df1883e"];
+      this.enterMarket();
+      this.mintCEth();
+      //this.mintCToken(this.cDAIInstance, 4*1e16);
+      this.redeemCEth(4*1e15);
+      this.setState({loaded:true});
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -35,36 +39,68 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
+  enterMarket = async () => {
+    const errorCode = await this.comptrollerInstance.methods.enterMarkets(this.cTokenAddresses).send({from: this.accounts[0]});
+    console.log("error code is: ");
+    console.log(errorCode);
+    const markets = await this.comptrollerInstance.methods.getAssetsIn(this.accounts[0]).call();
+    this.setState({marketAccounts:markets});
+    console.log("market is ");
+    console.log(markets);
   };
 
+  mintCEth = async () => {
+    const errorCode = await this.cETHInstance.methods.mint().send({from: this.accounts[0], value: (5*1e16)});
+    console.log("Minted result: ");
+    console.log(errorCode);
+    const balance = await this.cETHInstance.methods.balanceOfUnderlying(this.accounts[0]).call();
+    console.log("new balance");
+    console.log(balance/1e18);
+  };
+
+  mintCToken = async (cToken,amount) => {
+    //approve before minting
+    const errorCode = await cToken.methods.mint(amount).send({from: this.accounts[0]});
+    console.log("Minted result: ");
+    console.log(errorCode);
+  };
+
+  redeemCEth = async (amount) => {
+    const errorCode = await this.cETHInstance.methods.redeemUnderlying(amount).send({from: this.accounts[0]});
+    console.log("Redeem result: ");
+    console.log(errorCode);
+  };
+
+  redeemCToken = async (cToken, amount) => {
+    const errorCode = await cToken.methods.redeemUnderlying(amount).send({from: this.accounts[0]});
+    console.log("Redeem result: ");
+    console.log(errorCode);
+  };
+
+  balanceOfCEth = async () => {
+    const balance = await this.cETHInstance.balanceOfUnderlying(this.accounts[0]).call();
+    console.log("new balance of cEth");
+    console.log(balance/1e18);
+  }
+
+  balanceOfCToken = async (cToken) => {
+    const balance = await cToken.balanceOfUnderlying(this.accounts[0]).call();
+    console.log("new balance of token");
+    console.log(balance/1e18);
+  }
+
   render() {
-    if (!this.state.web3) {
+    if (!this.state.loaded) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>Compound Dashboard</h1>
+        <label>Total Markets entered : {this.state.marketAccounts}</label>
+        <div>
+        var array = {this.state.marketAccounts};
+        
+        </div>
       </div>
     );
   }
--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const { projectId, mnemonic } = require('./secrets.json');
+const HDWalletProvider = require('truffle-hdwallet-provider');
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,6 +9,19 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
+    },
+    rinkeby:{
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${projectId}`),
+      network_id: 4,       
+      gas: 5500000,        
+      confirmations: 1,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true 
     }
-  }
+  },
+  compilers: {
+    solc: {
+      version: "^0.5"
+    }
+ }
 };
